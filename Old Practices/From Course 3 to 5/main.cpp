1944f026aea3e9720553027dcd24c1946293e9c1
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-10-13 18:31:48 +0100

    This Is My First Lessons and Problem Soving Using C++ From Course 3 To 5 Back in 2022

---
+++ Old Practices/From Course 3 to 5/main.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{    
+    string name = "mohamad abou_hadhoud" ; 
+    unsigned short age = 44 ; 
+    string city = "amman";  
+    string country = "jordan"; 
+    float monthlysalary = 5000 ;
+    float yearlysalary = 6000 ;
+    unsigned char gender = 'm'; 
+    bool ismarried = true ; 
+      
+       
+    cout << "*************************************************\n"  ;  
+         cout << " name : " << name << endl; 
+         cout << " age : " << age << endl; 
+         cout << " city : " << city << endl; 
+         cout << " country : " << country <<endl; 
+         cout << " monthly salary : " << monthlysalary <<endl; 
+         cout << " yearly salary : " << yearlysalary << endl; 
+         cout << " gender : " << gender << endl; 
+         cout << " marriead : " << ismarried << endl;  
+    cout << "*****************************************************\n"   ;  
+          
+           
+           
+ 
+  
+    return 0;
+}
